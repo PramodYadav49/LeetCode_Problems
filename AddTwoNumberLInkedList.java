0178fc94da161064f28d3bef64cd64842a133362
PramodYadav49 <117937794+PramodYadav49@users.noreply.github.com>
2024-02-03 17:08:43 +0530

    Create AddTwoNumberLInkedList.java
    LeetCode problem number    2

---
+++ AddTwoNumberLInkedList.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+       int sum = 0;
+       int carry = 0;
+       ListNode NewList = new ListNode(0);
+       ListNode temp = NewList;
+       
+       while (l1 != null || l2 != null) {
+           if (l1 != null) {
+               sum += l1.val;
+               l1 = l1.next;
+           }
+           if (l2 != null) {
+               sum += l2.val;
+               l2 = l2.next;
+           }
+           sum += carry;
+           int res = sum % 10;
+           ListNode node = new ListNode(res);
+           temp.next = node;
+           temp = temp.next;
+           carry = sum / 10;
+           sum = 0; 
+       }
+       
+       if (carry > 0) {
+           temp.next = new ListNode(carry);
+       }
+       
+       return NewList.next;
+    }
+}
